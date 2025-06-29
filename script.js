fdcf0028ebb1d48c0068a82f1b974dd77456a58a
spin <tgvbnm7980@gmail.com>
2025-06-29 19:38:41 +0800

    1140629 衣服統計03 完整版03

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.querySelector('.submitButton').addEventListener('click', function () {
     const style = document.querySelector('input[name="style"]:checked');
     const color = document.querySelector('input[name="color"]:checked');
+    const size = document.querySelector('input[name="size"]:checked');
     const quantityInput = document.getElementById('quantity');
     const quantity = parseInt(quantityInput.value);
 
-    if (!style || !color || !quantity || quantity <= 0) {
+    if (!style || !color || !size || !quantity || quantity <= 0) {
         alert('請選擇衣服樣式、顏色並輸入正確數量');
         return;
     }
@@ -24,14 +25,23 @@ document.querySelector('.submitButton').addEventListener('click', function () {
         green: '綠色'
     };
 
+    const sizeTextMap = {
+        S: 'S號',
+        M: 'M號',
+        L: 'L號',
+        XL: 'XL號',
+        '2XL': '2XL號',
+    }
+
     let found = false;
     for (let row of tableBody.rows) {
         const styleText = row.cells[0].textContent;
         const colorText = row.cells[1].textContent;
+        const sizeText = row.cells[2].textContent;
 
-        if (styleText === styleTextMap[style.value] && colorText === colorTextMap[color.value]) {
-            let oldQty = parseInt(row.cells[2].textContent);
-            row.cells[2].textContent = oldQty + quantity;
+        if (styleText === styleTextMap[style.value] && colorText === colorTextMap[color.value] && sizeText === sizeTextMap[size.value]) {
+            let oldQty = parseInt(row.cells[3].textContent);
+            row.cells[3].textContent = oldQty + quantity;
             found = true;
             break;
         }
@@ -41,7 +51,8 @@ document.querySelector('.submitButton').addEventListener('click', function () {
         const newRow = tableBody.insertRow();
         newRow.insertCell(0).textContent = styleTextMap[style.value];
         newRow.insertCell(1).textContent = colorTextMap[color.value];
-        newRow.insertCell(2).textContent = quantity;
+        newRow.insertCell(2).textContent = sizeTextMap[size.value];
+        newRow.insertCell(3).textContent = quantity;
     }
 
     // 清除輸入
